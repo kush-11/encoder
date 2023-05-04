e877dc572c1c85f3fbc0ecbf0c2acfc3c037b416
Kush <kush1604.be20@chitkara.edu.in>
2023-05-04 13:05:07 +0530

    create login signup link

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -19,8 +19,8 @@ const NavBar = () => {
         />
       </Navbar.Brand>
       <Nav className="ms-auto">
-        <Nav.Link href="https://encoder-login-form.vercel.app/login">Login</Nav.Link>
-        <Nav.Link href="https://encoder-login-form.vercel.app/signup">Signup</Nav.Link>
+        <Nav.Link href="https://encoder-login-form-okm61fvej-kush-11.vercel.app/login">Login</Nav.Link>
+        <Nav.Link href="https://encoder-login-form-okm61fvej-kush-11.vercel.app/signup">Signup</Nav.Link>
         
       </Nav>
     </Navbar>
